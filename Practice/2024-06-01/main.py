0a690c9772fc0a39b67ca81b646e204fdb1acd6a
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-06-04 22:33:31 +0900

    Renamed folders

--- Practice/2024-06-01/main.py
+++ Practice/2024-06-01/main.py
@@ -1,11 +1,9 @@
 import sys
 
-# input.txt を読み込む
 input_file = 'input.txt'
 with open(input_file, 'r') as f:
     input_data = f.read()
 
-# 標準入力から読み込んだように処理する
 sys.stdin = open(input_file, 'r')
 
 # ここから通常のコード
