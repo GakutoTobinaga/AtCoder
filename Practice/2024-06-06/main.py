44f725a3a571d97000ac9f59e5a11179e55e2d8a
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-06-06 13:19:09 +0900

    Done Bootcamp

--- Practice/2024-06-06/main.py
+++ Practice/2024-06-06/main.py
@@ -4,7 +4,7 @@ with open(input_file, 'r') as f:
     input_data = f.read()
 
 sys.stdin = open(input_file, 'r')
-
+"""
 A, B = list(map(int, input().split()))
 
 N = 0
@@ -15,3 +15,20 @@ while N < B:
     if N < B:
         N -= 1
 print(T)
+"""
+
+N = int(input())
+Xs = list(map(int, input().split()))
+Xs_min = min(Xs)
+Xs_max = max(Xs)
+
+Answers = []
+Xs_set = list(set(Xs))
+for i in range(Xs_min, Xs_max + 1):
+    #開催地を移動する
+    total = 0
+    for j in range(len(Xs)):
+        #住人を一人ずつ今の開催地と計算して足す
+        total += (Xs[j] - i) ** 2
+    Answers.append(total)
+print(min(Answers))
